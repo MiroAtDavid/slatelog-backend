edfd4be55a8c5f89994e177de68912a42de15feb
Miro David <bluecloud@eclipso.me>
2024-02-18 17:35:43 +0100

    tests(findByTitle, registration fix)

--- slatelog/src/main/java/com/slatelog/slatelog/security/web/WebSecurityConfig.java
+++ slatelog/src/main/java/com/slatelog/slatelog/security/web/WebSecurityConfig.java
@@ -94,7 +94,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.DefaultSecurityFilterChain;
@@ -102,7 +101,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
