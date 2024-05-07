bf6c72dd7d88a73d1f12b986e38e8543f69de00b
MiroAtDavid <bluecloud@elcipso.me>
2024-05-07 12:53:53 +0200

    Invitation(pollOptions)

--- slatelog/src/main/java/com/slatelog/slatelog/domain/event/Invitation.java
+++ slatelog/src/main/java/com/slatelog/slatelog/domain/event/Invitation.java
@@ -1,8 +1,14 @@
 package com.slatelog.slatelog.domain.event;
 
+import com.slatelog.slatelog.security.token.InvitationEmailToken;
 import com.slatelog.slatelog.security.token.Token;
+import jakarta.annotation.Nullable;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import java.time.Duration;
+import java.time.Instant;
 
 // Class representing an invitation to an event
 @Getter
@@ -13,18 +19,34 @@ public class Invitation {
     private String email;
 
     // Token associated with the email for verification
-    private Token emailToken;
+    private @Nullable Token emailToken;
+
+    // Event invitation voting end date
+    private Instant tokenExpirationDate;
+
+    // TODO We need to get this from the poll
+    private boolean pollIsOpen;
 
     // Constructor to create an Invitation with an email and token
     public Invitation(String email) {
         this.email = email;
-        this.emailToken = emailToken;
+        // TODO implement event poll voting endTime
+        setEmailToken(email);
     }
 
-    // Default constructor (no-args constructor) for Springboot
-    // It's marked as protected to limit its accessibility
+    // Default constructor (
     protected Invitation(){};
 
+    private void setEmailToken(String email){
+        // TODO implement event poll voting endTime
+        // TODO implement event poll boolean isOpen()
+        if (!pollIsOpen){
+            emailToken = null;
+        } else {
+            this.emailToken = InvitationEmailToken.generateInvitationToken(email, Instant.now().plus(Duration.ofHours(24)));
+        }
+
+    }
 
 
 
